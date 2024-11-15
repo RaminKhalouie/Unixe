f241e97218704ec2abb80a0a38faa5876a119e7c
Ramin Khalouie <ra.khalouie@gmail.com>
2024-11-15 17:58:55 +0330

    [FIX] : Fix some typo
    Fix the name convention in lg_back to lg-back

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/git**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
         "hero_bg": "url('../images/bg-hero.png')",
-        "logo_bg": "url(../images/lg_back.svg)"
+        "logo_bg": "url(../images/lg-back.svg)"
       }
     },
   },
